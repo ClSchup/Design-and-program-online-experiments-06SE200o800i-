0f56034ff4e4136f21ef232cbecca25be335a926
ClSchup <claudia.schuppisser@hotmail.com>
2026-03-17 10:43:10 +0100

    mend session5 html

---
+++ Session5_STARTER_Claudia/STARTER/session5.js
@@ -0,0 +1,22 @@
+let ageGroup;
+const age = 10;
+if (age < 18) {
+ageGroup = 'Child';
+} else {
+ageGroup = 'Adult';}
+
+console.log("ageGroup: ", ageGroup);    
+
+let age = 18
+let ageGroup = age < 18 ? 'child' : 'adult';
+console.log("ageGroup: ", ageGroup);  
+
+
+/* Example */
+for (let i = 0; i < 5; i++) {
+// console.log(`Number: ${i}`);
+console.log("Number: ", i);}
+
+
+for (let i = 0; i < 5; i++) {
+console.log("i/number:" ,i);}
